b1a85b2904cc437735c381f17e698115780a8052
grj <3303272844@qq.com>
2022-08-13 17:32:50 +0800

    用户页面基本实现

--- src/module-manage/components/user-add.vue
+++ src/module-manage/components/user-add.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="add-form">
-    <el-dialog :title="text+pageTitle" :visible.sync="dialogFormVisible">
+    <el-dialog :title="dialogTitle" :visible="visible" @close="onClose">
       <el-form
         :rules="ruleInline"
         ref="dataForm"
         :model="formBase"
         label-position="left"
         label-width="120px"
-        style="width: 400px; margin-left:120px;"
+        style="width: 400px; margin-left: 120px"
       >
-
         <el-form-item :label="$t('table.username')" prop="username">
           <el-input v-model="formBase.username"></el-input>
         </el-form-item>
@@ -19,7 +18,7 @@
         <el-form-item
           :label="$t('table.paddword')"
           prop="password"
-          v-if="formBase.password!=undefined"
+          v-if="formBase.password != undefined"
         >
           <el-input v-model="formBase.password"></el-input>
         </el-form-item>
@@ -29,14 +28,17 @@
           <el-input v-model="formBase.role"></el-input>
         </el-form-item>
         <!-- 权限组 -->
-        <el-form-item :label="$t('table.permissionUser')" prop="permission_group_id">
+        <el-form-item
+          :label="$t('table.permissionUser')"
+          prop="permission_group_id"
+        >
           <el-select class="filter-item" v-model="formBase.permission_group_id">
-            <el-option
+            <!-- <el-option
               v-for="item in PermissionGroupsList"
               :value="item.id"
               :key="item.key"
               :label="item.title"
-            ></el-option>
+            ></el-option> -->
           </el-select>
         </el-form-item>
 
@@ -44,99 +46,112 @@
           <el-input v-model="formBase.phone"></el-input>
         </el-form-item>
 
-        <!-- 头像上传下一个版本再做 -->
-        <!-- <el-form-item :label="$t('table.avatar')" prop="avatar">
-            <el-upload
-              class="upload-demo"
-              :action="importFileUrl"
-              :on-change="handleChange"
-              :file-list="fileList" accept="image/jpeg,image/gif,image/png,image/bmp"
-              :before-upload="beforeAvatarUpload">
-              <el-button size="small" type="primary">点击上传</el-button>
-              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-            </el-upload>
-        </el-form-item>-->
         <el-form-item :label="$t('table.introduction')">
           <el-input
             type="textarea"
-            :autosize="{ minRows: 2, maxRows: 4}"
+            :autosize="{ minRows: 2, maxRows: 4 }"
             placeholder="Please input"
             v-model="formBase.introduction"
           ></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="handleClose">{{$t('table.cancel')}}</el-button>
-        <el-button type="primary" @click="createData">{{$t('table.confirm')}}</el-button>
+        <el-button @click="onClose">{{ $t("table.cancel") }}</el-button>
+        <el-button type="primary" @click="createData">{{
+          $t("table.confirm")
+        }}</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { detail, update, add } from '@/api/base/users'
+import { detail, update, add } from "@/api/base/users";
 export default {
-  name: 'usersAdd',
-  props: [
-    'text',
-    'pageTitle',
-    'PermissionGroupsList',
-    'formBase',
-    'ruleInline'
-  ],
-  data () {
-    return {
-      dialogFormVisible: false
-      // fileList: [],
-      // importFileUrl: 'https://jsonplaceholder.typicode.com/posts/',
-    }
+  name: "usersAdd",
+  props: {
+    // 控制新增的弹窗显示隐藏
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    // 控制修改的弹窗显示隐藏
+    visiabledia: {
+      type: Boolean,
+    },
   },
-  computed: {},
+  data() {
+    return {
+      formBase: {
+        id: "",
+        username: "",
+        email: "",
+        password: "",
+        role: "",
+        permission_group_id: "",
+        phone: "",
+        introduction: "",
+      },
+      ruleInline: {
+        username: [
+          { required: true, message: "用户名不能为空", trigger: "blur" },
+        ],
+        email: [{ required: true, message: "邮箱不能为空", trigger: "blur" }],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+        ],
+        role: [{ required: true, message: "角色不能为空", trigger: "blur" }],
+        phone: [
+          { required: true, message: "联系电话不能为空", trigger: "blur" },
+        ],
+        introduction: [
+          { required: true, message: "介绍不能为空", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  computed: {
+    dialogTitle() {
+      return this.visiabledia ? "编辑用户" : "创建用户";
+    },
+  },
   methods: {
-    // 弹层显示
-    dialogFormV () {
-      this.dialogFormVisible = true
-    },
-    // 弹层隐藏
-    dialogFormH () {
-      this.dialogFormVisible = false
-    },
-    // 退出
-    handleClose () {
-      this.$emit('handleCloseModal')
-    },
-
     // 表单提交
-    createData () {
-      this.$refs.dataForm.validate(valid => {
-        if (valid) {
-          this.$emit('handleCloseModal')
-          const data = {
-            ...this.formBase
-          }
-          if (this.formBase.id) {
-            update(data).then(() => {
-              this.$emit('newDataes', this.formBase)
-            })
-          } else {
-            add(this.formBase).then(() => {
-              this.$emit('newDataes', this.formBase)
-            })
-          }
+    createData() {
+      this.$refs.dataForm.validate();
+      if (this.visiabledia) {
+        if (this.formBase.id) {
+          update(data).then(() => {
+            this.$emit("newDataes", this.formBase);
+          });
         } else {
-          this.$Message.error('*号为必填项!')
+          add(this.formBase).then(() => {
+            this.$emit("newDataes", this.formBase);
+          });
         }
-      })
-    }
+      } else {
+        this.$Message.error("*号为必填项!");
+      }
+    },
+    // 关闭弹层
+    onClose() {
+      this.$emit("update:visible", false);
+      this.formBase = "";
+    },
+    //编辑数据回显
+    async getUpdat(val) {
+      console.log(val);
+      this.formBase = val;
+    },
   },
   // 挂载结束
 
   mounted: function () {},
   // 创建完毕状态
-  created () {},
+  created() {},
   // 组件更新
-  updated: function () {}
-}
+  updated: function () {},
+};
 </script>
 <style>
 .el-form--label-left .el-form-item__label {
@@ -146,6 +161,6 @@ export default {
   margin-bottom: 22px;
 }
 .el-dialog__footer {
-  text-align: center
+  text-align: center;
 }
 </style>
--- src/module-manage/pages/users.vue
+++ src/module-manage/pages/users.vue
@@ -1,27 +1,148 @@
 <template>
-  <div>
-    用户列表
+  <div class="app-container">
+    <el-card shadow="always">
+      <!-- 头部搜索框跟警告 -->
+      <UserHeader>
+        <template #right>
+          <el-button
+            type="success"
+            icon="el-icon-edit"
+            size="small"
+            @click="AddUser"
+            >新增用户</el-button
+          >
+        </template>
+        <template #left-tag>
+          <span>共{{ counts }}条记录</span>
+        </template>
+      </UserHeader>
+      <!-- 表格主体 -->
+      <el-table :data="tableData" style="width: 100%" v-loading="loading">
+        <el-table-column prop="id" label="序号" width="100px">
+        </el-table-column>
+        <el-table-column prop="email" label="邮箱"> </el-table-column>
+        <el-table-column prop="phone" label="联系电话"> </el-table-column>
+        <el-table-column prop="username" label="用户名"> </el-table-column>
+        <el-table-column prop="permission_group_title" label="权限组名称">
+        </el-table-column>
+        <el-table-column prop="role" label="角色"> </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="{ row }">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              circle
+              plain
+              @click="EditUser(row)"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              circle
+              plain
+              @click="deleteUser(row)"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <PageTool
+        :total="counts"
+        @pageChange="pageChange"
+        @pageSizeChange="pageSizeChange"
+      ></PageTool>
+
+      <!-- 新增编辑弹框 -->
+      <UserAdd
+        ref="useradd"
+        :visible.sync="dialogFormVisible"
+        :visiabledia.sync="logVisible"
+        @newDataes="getUserList"
+      ></UserAdd>
+    </el-card>
   </div>
 </template>
 
 <script>
+import UserAdd from "@/module-manage/components/user-add.vue";
+import UserHeader from "@/module-manage/components/user-header.vue";
+import PageTool from "@/module-manage/components/page-tool.vue";
+import { list, remove, simple } from "@/api/base/users.js";
 export default {
-  data () {
+  data() {
     return {
-
-    }
+      tableData: [], //用户列表数据
+      counts: 0, //记录条数
+      params: {
+        page: 1,
+        pagesize: 10,
+      },
+      loading: false,
+      dialogFormVisible: false,
+      logVisible: false,
+      PermissionGroupsList: [],
+    };
+  },
+  components: {
+    UserHeader,
+    PageTool,
+    UserAdd,
   },
 
-  created () {
-
+  created() {
+    this.getUserList();
   },
 
   methods: {
-
-  }
-}
+    // 获取用户列表数据
+    async getUserList() {
+      // 加载
+      this.loading = true;
+      const res = await list(this.params);
+      // console.log(res);
+      this.tableData = res.data.list;
+      this.counts = res.data.counts;
+      this.loading = false;
+    },
+    pageChange() {},
+    pageSizeChange() {},
+    // 删除用户
+    async deleteUser(row) {
+      try {
+        await this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        });
+        console.log(row);
+        await remove(row.id);
+        this.$message({
+          message: "恭喜你，删除成功",
+          type: "success",
+        });
+        this.getUserList();
+      } catch (error) {}
+    },
+    // 编辑用户
+    EditUser(row) {
+      this.logVisible = true;
+      this.dialogFormVisible = true;
+      this.$refs.useradd.getUpdat(row);
+    },
+    //创建用户
+    AddUser() {
+      this.dialogFormVisible = true;
+      this.logVisible = false;
+    },
+  },
+};
 </script>
 
-<style scoped lang='less'>
-
+<style scoped>
+.app-container {
+  padding: 20px;
+}
+.el-table th {
+  background-color: #fafafa;
+}
 </style>
