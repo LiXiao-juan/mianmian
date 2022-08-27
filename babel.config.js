578c7961bbe9bfaa76e60e3bff55cd66b2a12a02
Hello-XY <1661230401@qq.com>
2022-08-27 09:19:14 +0800

    修复题库切换多选框不恢复初始化状态

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+let prodPlugins = [];
+if (process.env.NODE_ENV === "production") {
+  prodPlugins = ["transform-remove-console"];
 }
+module.exports = {
+  presets: ["@vue/cli-plugin-babel/preset"],
+  env: {
+    development: {
+      plugins: ["dynamic-import-node", ...prodPlugins],
+    },
+  },
+};
